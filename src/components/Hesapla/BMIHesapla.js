08807a9415e1d90b5969b9da8a3d28cd510810f8
Neim-Ramazanoglu <mr.ramazanoglu@gmail.com>
2021-06-16 13:15:16 +0300

    son güncelleme

--- src/components/Hesapla/BMIHesapla.js
+++ src/components/Hesapla/BMIHesapla.js
@@ -34,15 +34,11 @@ function Hesapla() {
     });
     const setValue = (value) => {
       try {
-        // Allow value to be a function so we have same API as useState
         const valueToStore =
           value instanceof Function ? value(storedValue) : value;
-        // Save state
         setStoredValue(valueToStore);
-        // Save to local storage
         window.localStorage.setItem(key, JSON.stringify(valueToStore));
       } catch (error) {
-        // A more advanced implementation would handle the error case
         console.log(error);
       }
     };
